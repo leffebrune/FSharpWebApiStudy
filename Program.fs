fb7a5fefaf3ce047ae39e70cfa607e2b6fe959a8
Dongsup Jung <mutebeholder@gmail.com>
2022-08-11 02:02:59 +0900

    NEW: initial commit

---
+++ Program.fs
@@ -0,0 +1,52 @@
+namespace FSharpWebApiStudy
+#nowarn "20"
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy
+open Microsoft.AspNetCore.StaticFiles
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.FileProviders
+
+
+module Program =
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+
+        let option = new WebApplicationOptions(Args = args, WebRootPath = "public")
+        
+        let builder = WebApplication.CreateBuilder(option)
+
+        builder.Services.AddControllers()
+        builder.Services.AddDirectoryBrowser()
+
+        let app = builder.Build()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        let fileProvider = new PhysicalFileProvider(builder.Environment.WebRootPath);
+        app.UseStaticFiles(new StaticFileOptions(
+            FileProvider = fileProvider
+        ));
+
+        app.UseDirectoryBrowser(new DirectoryBrowserOptions(
+            FileProvider = fileProvider
+        ));
+
+
+        app.Run()
+
+        exitCode
